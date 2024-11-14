eeb265fbaef1601126964c8f067f17727bca7652
Adrien Schmidt <61613165+Adrien-25@users.noreply.github.com>
2024-11-14 18:09:48 +0100

    finish auth settings

--- src/main/java/com/example/Todo_app_spring/config/SecurityConfig.java
+++ src/main/java/com/example/Todo_app_spring/config/SecurityConfig.java
@@ -3,9 +3,10 @@ package com.example.Todo_app_spring.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -28,13 +29,13 @@ public class SecurityConfig {
         return userService;
     }
 
-    // @Bean
-    // public AuthenticationProvider authenticationProvider() {
-    //     DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-    //     provider.setUserDetailsService(userService);
-    //     provider.setPasswordEncoder(passwordEncoder());
-    //     return provider;
-    // }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -44,16 +45,30 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .csrf(AbstractHttpConfigurer::disable)
+                // .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf
+                .ignoringRequestMatchers("/h2-console/**")
+                .disable()
+                )
+                .headers(headers -> headers
+                .frameOptions(frameOptions -> frameOptions.sameOrigin())
+                )
                 .formLogin(httpForm -> {
                     httpForm.loginPage("/req/login").permitAll();
-                    httpForm.defaultSuccessUrl("/index");
+                    httpForm.defaultSuccessUrl("/", true);
 
                 })
                 .authorizeHttpRequests(registry -> {
-                    registry.requestMatchers("/req/signup", "/css/**", "/js/**").permitAll();
+                    registry.requestMatchers("/req/signup", "/css/**", "/js/**", "/h2-console/**").permitAll();
                     registry.anyRequest().authenticated();
                 })
+                .logout(logout -> logout
+                .logoutUrl("/auth/logout")
+                .logoutSuccessUrl("/auth/login?logout")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .permitAll()
+                )
                 .build();
     }
 }
--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const signupForm = document.querySelector('section');
-    signupForm.style.opacity = 0;
+    // const signupForm = document.querySelector('section');
+    // signupForm.style.opacity = 0;
   
-    setTimeout(() => {
-      signupForm.style.transition = 'opacity 1s ease-in-out';
-      signupForm.style.opacity = 1;
-    }, 500);
+    // setTimeout(() => {
+    //   signupForm.style.transition = 'opacity 1s ease-in-out';
+    //   signupForm.style.opacity = 1;
+    // }, 500);
   
     const signupButton = document.querySelector('button');
     signupButton.addEventListener('click', function () {
