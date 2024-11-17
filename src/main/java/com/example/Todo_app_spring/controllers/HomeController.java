7ac7a3e94877191b57aae8e3998a93619c921c38
Adrien Schmidt <61613165+Adrien-25@users.noreply.github.com>
2024-11-17 04:29:28 +0100

    start design odo app

--- src/main/java/com/example/Todo_app_spring/controllers/HomeController.java
+++ src/main/java/com/example/Todo_app_spring/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package com.example.Todo_app_spring.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,6 +19,16 @@ public class HomeController {
     public ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("todoItems", todoItemService.getAll());
+
+        // Récupérer l'authentification actuelle
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // Vérifier si l'utilisateur est authentifié
+        if (authentication != null && authentication.isAuthenticated() && !"anonymousUser".equals(authentication.getPrincipal())) {
+            String username = authentication.getName();
+            modelAndView.addObject("username", username);
+        }
+
         return modelAndView;
     }
 
