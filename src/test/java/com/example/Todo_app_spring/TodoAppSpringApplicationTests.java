155128811b02e597bd492a950ffdd349031cc649
Adrien Schmidt <61613165+Adrien-25@users.noreply.github.com>
2024-11-19 02:29:35 +0100

    change dockerfile for deployement 7

--- src/test/java/com/example/Todo_app_spring/TodoAppSpringApplicationTests.java
+++ src/test/java/com/example/Todo_app_spring/TodoAppSpringApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.Todo_app_spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TodoAppSpringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Ce test vérifie simplement que le contexte de l'application se charge correctement
+    }
 }
