7c218f001622fcb9b3d1a0f73602e1230d4eb0fd
Adrien Schmidt <61613165+Adrien-25@users.noreply.github.com>
2024-11-29 18:35:45 +0100

    responsive sidebar

--- src/main/resources/static/js/homepage.js
+++ src/main/resources/static/js/homepage.js
@@ -1,9 +1,13 @@
 // Sélection des éléments
 const toggleButton = document.getElementById("toggle-sidebar");
 const sidebar = document.getElementById("sidebar");
+const closeButton = document.getElementById("close-sidebar");
 
 // Ajouter un écouteur d'événement au bouton
 toggleButton.addEventListener("click", () => {
+  sidebar.classList.toggle("close"); // Ajouter ou retirer la classe 'open'
+});
+closeButton.addEventListener("click", () => {
   sidebar.classList.toggle("close"); // Ajouter ou retirer la classe 'open'
 });
 
@@ -12,16 +16,10 @@ toggleButton.addEventListener("click", () => {
 
 // inputIsComplete
 const taskCheckboxes = document.querySelectorAll(".task-checkbox");
-// console.log(taskCheckboxes);
 taskCheckboxes.forEach((checkbox) => {
   checkbox.addEventListener("change", function () {
-    // Récupère l'ID du formulaire associé à cette checkbox
     const formId = `todoForm-${this.dataset.id}`;
-    console.log(formId);
-    // Soumet le formulaire associé
     const form = document.getElementById(formId);
-    console.log(form);
-
     form.submit();
   });
 });
